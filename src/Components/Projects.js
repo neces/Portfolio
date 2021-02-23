e520bcfdbb97d1fe79c9b6208d78143c7ba6635f
Neja's Mac <neja.cesnik@hotmail.com>
2021-02-23 22:49:04 +0100

    Spacing with subtitles

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,7 +10,7 @@ const Projects = ({ }) => {
   return (
     <div className='projects'>
       <Container>
-      <Grid container spacing={8}
+      <Grid container spacing={6}
         direction="row"
         justify="center"
         alignItems="flex-start"
@@ -19,26 +19,16 @@ const Projects = ({ }) => {
           <div><img src={DesignSprintDashboard} width="550" alt="Final year project Design Sprint dahsboard" /></div>
           <h3>FINAL YEAR PROJECT</h3>
           <h1>Web Application to Support the Design Sprint Process</h1>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <div><img src={OpenDayDashboard} width="550" alt="Virtual open day dashboard" /></div>
-          <h3>INTERACTIVE DESIGN</h3>
-          <h1>Virtual University Open Day Concept</h1>
-        </Grid>
-        </Grid>
-        <Grid container spacing={6}
-        direction="row"
-        justify="center"
-        alignItems="flex-start"
-      >
-        <Grid item xs={12} sm={6}>
           <div className='subtitle'>
             <p>Project that supports the planning stage of a Design Sprint and helps facilitators focus on the people, rather than the process.</p>
             <h3>DESIGN THINKING ⋅ USER RESEARCH ⋅ REQUIREMENTS GATHERING ⋅ PROTOTYPING ⋅ USER EXPERIENCE DESIGN ⋅ JAVASCRIPT ⋅ REACT ⋅ NODE.JS</h3>
           </div>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <div className='subtitle'>
+        <div><img src={OpenDayDashboard} width="550" alt="Virtual open day dashboard" /></div>
+          <h3>INTERACTIVE DESIGN</h3>
+          <h1>Virtual University Open Day Concept</h1>
+          <div className='subtitle-extra-space'>
             <p>Ideating with a Design Sprint to create an interactive solution for bringing the University Open Day experience to people remotely.</p>
             <h3>DESIGN THINKING ⋅ DESIGN SPRINT ⋅ MIRO ⋅ USER EXPERIENCE ⋅ INTERACTIVE DESIGN ⋅ DESIGN SYSTEM ⋅ ADOBE XD</h3>
         </div>
